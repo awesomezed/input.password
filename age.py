cf87ca57fddce43d21a343b1846e2231da810905
MD ZIAUR RAHMAN <64756891+awesomezed@users.noreply.github.com>
2020-08-18 00:32:08 +0600

    Add files via upload
    This code is not running as i expected ,if the password is correct its not going according to i wrote...

---
+++ age.py
@@ -0,0 +1,20 @@
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+
+LEGAL_AGE=18
+age = int(input("Please enter your age: "))
+# do exception handling to make sure age is in integer format
+if age<18 :
+    print("GO HOME")
+else:
+
+    password =input("TYPE YOUR PASSWORD : ")
+
+
+    if password!=int(12345):
+
+
+        print('WRONG PASSWORD')
+    else :
+        print('YES , YOU CAN ENTER NOW')
